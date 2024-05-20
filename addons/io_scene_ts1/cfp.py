3ed5213077692a19e6476ea77ababc7a8f3626cb
mix <mixsims@proton.me>
2024-05-20 01:55:44 +0100

    feat: compress cfp values with deltas

--- addons/io_scene_ts1/cfp.py
+++ addons/io_scene_ts1/cfp.py
@@ -1,6 +1,9 @@
 import math
 import struct
 
+def decode_delta(delta):
+    return 3.9676e-10 * math.pow(float(delta) - 126.0, 3) * abs(float(delta) - 126.0)
+
 def decode_values(file, count):
     values = list()
 
@@ -16,11 +19,18 @@ def decode_values(file, count):
             for i in range(repeat_count + 1):
                 values.append(previous_value)
         else: # delta
-            values.append(previous_value + 3.9676e-10 * math.pow(float(compression_type) - 126.0, 3) * abs(float(compression_type) - 126.0))
+            values.append(previous_value + decode_delta(compression_type))
 
         previous_value = values[-1]
 
     return values
+
+
+def create_delta_table():
+    delta_table = list()
+    for i in range(253):
+        delta_table.append(decode_delta(i))
+    return delta_table
 
 
 def encode_value_full(value):
@@ -31,37 +41,58 @@ def encode_value_repeat(repeat_count):
     return bytes([0xFE]) + struct.pack('<H', repeat_count - 1)
 
 
+def encode_value_delta(delta):
+    return struct.pack('<B', delta)
+
+
 def encode_values(values, compress):
+    delta_table = create_delta_table()
+
     encoded_bytes = bytes()
 
-    if compress:
-        previous_value = float('inf')
-        repeat_count = 0
-        for value in values:
+    values = iter(values)
+    previous_value = next(values)
+    encoded_bytes += encode_value_full(previous_value)
+
+    repeat_count = 0
+    for value in values:
+        difference = value - previous_value
+        delta_index, delta = min(enumerate(delta_table), key=lambda x: abs(x[1] - difference))
+
+        if compress:
+            if delta_index >= 120 and delta_index < 133 and repeat_count < 65535:
+                # if the sign of the value changes, we need to make sure
+                # that this change is outputted for the quaternions to work
+                if delta_index != 126 and not (value * previous_value >= 0.0):
+                    delta_index = 133 if value >= 0.0 else 119
+                    delta = delta_table[delta_index]
+                else:
+                    repeat_count += 1
+                    continue
+        else:
             if value == previous_value and repeat_count < 65535:
                 repeat_count += 1
                 continue
 
-            if repeat_count > 0:
-                encoded_bytes += encode_value_repeat(repeat_count)
-                repeat_count = 0
-
-            encoded_bytes += encode_value_full(value)
-
-            previous_value = value
-
         if repeat_count > 0:
             encoded_bytes += encode_value_repeat(repeat_count)
-    else:
-        for value in values:
+            repeat_count = 0
+
+        delta_difference = abs(difference - delta)
+        if not compress or delta_difference > 0.001:
             encoded_bytes += encode_value_full(value)
+            previous_value = value
+        else:
+            encoded_bytes += encode_value_delta(delta_index)
+            previous_value += delta
+
+    if repeat_count > 0:
+        encoded_bytes += encode_value_repeat(repeat_count)
 
     return encoded_bytes
 
 
 def read_file(file_path, position_count, rotation_count):
-    values = {}
-
     file = open(file_path, mode='rb')
 
     decoded_values = decode_values(file, (position_count * 3) + (rotation_count * 4))
@@ -71,6 +102,8 @@ def read_file(file_path, position_count, rotation_count):
         raise Exception("data left unread at end of cfp file")
     except:
         pass
+
+    values = {}
 
     values["positions_x"] = decoded_values[:position_count]
     values["positions_y"] = decoded_values[position_count:position_count * 2]
