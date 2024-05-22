da9e3b75363c062552e9216f0e19029bf1a71390
mix <mixsims@proton.me>
2024-05-22 17:16:45 +0100

    fix: load universal hand textures for all hand sex

--- addons/io_scene_ts1/import_ts1.py
+++ addons/io_scene_ts1/import_ts1.py
@@ -33,11 +33,12 @@ def create_texture_file_name_variants(skin_name, preferred_skin_color):
         hand = model_type[2]
         position = model_type[3]
 
-        for side in ["a", "l", "r"]:
-            for skin_color in skin_colors:
-                texture_names.append(("h" + sex + side + position + skin_color).lower())
-            texture_names.append(("h" + sex + side + position).lower())
-            texture_names.append(("g" + sex + side + position).lower())
+        for sex in ["u", sex.lower()]:
+            for side in ["a", "l", "r"]:
+                for skin_color in skin_colors:
+                    texture_names.append(("h" + sex + side + position + skin_color).lower())
+                texture_names.append(("h" + sex + side + position).lower())
+                texture_names.append(("g" + sex + side + position).lower())
     else:
         for skin_color in skin_colors:
             # TYPE_IDENTIFER<skin_color>
