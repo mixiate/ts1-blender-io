25332674852bbded4045bbdbb16c5bb6b88c56cf
mix <mixsims@proton.me>
2024-05-30 00:41:25 +0100

    style: add annotations and comments to skn module

--- addons/io_scene_ts1/skn.py
+++ addons/io_scene_ts1/skn.py
@@ -1,39 +1,47 @@
+"""Read and write The Sims 1 SKN files."""
+
+import pathlib
+import typing
+
+
 from . import bmf
 
 
-def read_bones(file):
+def read_bones(file: typing.TextIO) -> list[str]:
+    """Read SKN bones."""
     count = int(file.readline())
-    bones = []
-    for i in range(count):
-        bones.append(file.readline().strip())
-    return bones
+    return [file.readline().strip() for _ in range(count)]
 
 
-def write_bones(file, bones):
+def write_bones(file: typing.TextIO, bones: list[str]) -> None:
+    """Write SKN bones."""
     file.write(str(len(bones)) + "\n")
     for bone in bones:
         file.write(bone + "\n")
 
 
-def read_faces(file):
+def read_faces(file: typing.TextIO) -> list[tuple[int, int, int]]:
+    """Read SKN faces."""
     count = int(file.readline())
     faces = []
-    for i in range(count):
+    for _ in range(count):
         values = file.readline().split(" ")
         faces.append((int(values[0]), int(values[1]), int(values[2])))
     return faces
 
 
-def write_faces(file, faces):
+def write_faces(file: typing.TextIO, faces: list[tuple[int, int, int]]) -> None:
+    """Write SKN faces."""
     file.write(str(len(faces)) + "\n")
     for face in faces:
         file.write("{} {} {}\n".format(*face))
 
 
-def read_bone_bindings(file):
+def read_bone_bindings(file: typing.TextIO) -> list[bmf.BoneBinding]:
+    """Read SKN bone bindings."""
     count = int(file.readline())
     bone_bindings = []
-    for i in range(count):
+    for _ in range(count):
         values = file.readline().split(" ")
         bone_bindings.append(
             bmf.BoneBinding(
@@ -42,77 +50,89 @@ def read_bone_bindings(file):
                 int(values[2]),
                 int(values[3]),
                 int(values[4]),
-            )
+            ),
         )
     return bone_bindings
 
 
-def write_bone_bindings(file, bone_bindings):
+def write_bone_bindings(file: typing.TextIO, bone_bindings: list[bmf.BoneBinding]) -> None:
+    """Write SKN bone bindings."""
     file.write(str(len(bone_bindings)) + "\n")
     for bone_binding in bone_bindings:
-        file.write(f"{bone_binding.bone_index}\n")
-        file.write(f"{bone_binding.vertex_index}\n")
-        file.write(f"{bone_binding.vertex_count}\n")
-        file.write(f"{bone_binding.blended_vertex_index}\n")
-        file.write(f"{bone_binding.blended_vertex_count}\n")
+        file.write(
+            "{} {} {} {} {}\n".format(  # noqa: UP032
+                bone_binding.bone_index,
+                bone_binding.vertex_index,
+                bone_binding.vertex_count,
+                bone_binding.blended_vertex_index,
+                bone_binding.blended_vertex_count,
+            ),
+        )
 
 
-def read_uvs(file):
+def read_uvs(file: typing.TextIO) -> list[tuple[float, float]]:
+    """Read SKN uvs."""
     count = int(file.readline())
     uvs = []
-    for i in range(count):
+    for _ in range(count):
         values = file.readline().split(" ")
         uvs.append((float(values[0]), float(values[1])))
     return uvs
 
 
-def write_uvs(file, uvs):
+def write_uvs(file: typing.TextIO, uvs: list[tuple[float, float]]) -> None:
+    """Write SKN uvs."""
     file.write(str(len(uvs)) + "\n")
     for uv in uvs:
         file.write("{:.7f} {:.7f}\n".format(*uv))
 
 
-def read_blends(file):
+def read_blends(file: typing.TextIO) -> list[bmf.Blend]:
+    """Read SKN blends."""
     count = int(file.readline())
     blends = []
-    for i in range(count):
+    for _ in range(count):
         values = file.readline().split(" ")
         blends.append(
             bmf.Blend(
                 int(values[1]),
                 int(values[0]),
-            )
+            ),
         )
     return blends
 
 
-def write_blends(file, blends):
+def write_blends(file: typing.TextIO, blends: list[bmf.Blend]) -> None:
+    """Write SKN blends."""
     file.write(str(len(blends)) + "\n")
     for blend in blends:
         file.write(f"{blend.vertex_index} {blend.weight}\n")
 
 
-def read_vertices(file):
+def read_vertices(file: typing.TextIO) -> list[bmf.Vertex]:
+    """Read SKN vertices."""
     count = int(file.readline())
     vertices = []
-    for i in range(count):
+    for _ in range(count):
         values = file.readline().split(" ")
         vertices.append(
             bmf.Vertex(
                 (float(values[0]), float(values[1]), float(values[2])),
                 (float(values[3]), float(values[4]), float(values[5])),
-            )
+            ),
         )
     return vertices
 
 
-def write_vertices(file, vertices):
+def write_vertices(file: typing.TextIO, vertices: list[bmf.Vertex]) -> None:
+    """Write SKN vertices."""
     file.write(str(len(vertices)) + "\n")
     for vertex in vertices:
         file.write("{:.7f} {:.7f} {:.7f} {:.7f} {:.7f} {:.7f}\n".format(*vertex.position, *vertex.normal))
 
 
-def read_skn(file):
+def read_skn(file: typing.TextIO) -> bmf.Bmf:
+    """Read SKN."""
     return bmf.Bmf(
         file.readline().strip(),
         file.readline().strip(),
@@ -125,7 +145,8 @@ def read_skn(file):
     )
 
 
-def write_skn(file, bmf):
+def write_skn(file: typing.TextIO, bmf: bmf.Bmf) -> None:
+    """Write SKN."""
     file.write(bmf.skin_name + "\n")
     file.write(bmf.default_texture_name + "\n")
     write_bones(file, bmf.bones)
@@ -136,16 +157,18 @@ def write_skn(file, bmf):
     write_vertices(file, bmf.vertices)
 
 
-def read_file(file_path):
+def read_file(file_path: pathlib.Path) -> bmf.Bmf:
+    """Read a file as a SKN."""
     with file_path.open() as file:
         skn = read_skn(file)
 
         if file.readline() != "":
-            raise Exception("data left unread at end of " + file_path)
+            raise Exception("data left unread at end of " + file_path.as_posix())
 
         return skn
 
 
-def write_file(file_path, bmf):
+def write_file(file_path: pathlib.Path, bmf: bmf.Bmf) -> None:
+    """Write a SKN to a file."""
     with file_path.open('w') as file:
         write_skn(file, bmf)
