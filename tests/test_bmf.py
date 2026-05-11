28a1b1a8ac89231601a8d9932863e3e83d63e634
mix <mixiate@proton.me>
2026-05-11 16:05:27 +0100

    test: write and reread memory stream instead of file in bmf test

--- tests/test_bmf.py
+++ tests/test_bmf.py
@@ -1,6 +1,6 @@
 """BMF format tests."""
 
-import itertools
+import io
 import multiprocessing
 from pathlib import Path
 import pytest
@@ -8,19 +8,22 @@ import pytest
 from ts1_formats import bmf
 
 
-def roundtrip_bmf(file_path: Path, tmp_path: Path) -> None:
+def roundtrip_bmf(file_path: Path) -> None:
     """Test reading, writing and rereading a bmf file."""
     bmf_file = bmf.read_file(file_path)
 
-    output_file_path = tmp_path / file_path.name
+    byte_stream = io.BytesIO()
+    bmf.write_bmf(byte_stream, bmf_file)
 
-    bmf.write_file(output_file_path, bmf_file)
-    output_bmf_file = bmf.read_file(output_file_path)
+    assert byte_stream.tell() == file_path.stat().st_size
+
+    byte_stream.seek(0)
+    output_bmf_file = bmf.read_bmf(byte_stream)
 
     assert bmf_file == output_bmf_file
 
 
-def test_bmf(tmp_path: Path, files_directory: str | None) -> None:
+def test_bmf(files_directory: str | None) -> None:
     """Test reading, writing and rereading all bmf files in the specified directory."""
     if files_directory is None:
         pytest.skip("No file directory specified")
@@ -28,4 +31,4 @@ def test_bmf(tmp_path: Path, files_directory: str | None) -> None:
     file_list = Path(files_directory).rglob("*.bmf")
 
     pool = multiprocessing.Pool(None)
-    pool.starmap(roundtrip_bmf, zip(file_list, itertools.repeat(tmp_path)))
+    pool.map(roundtrip_bmf, file_list)
