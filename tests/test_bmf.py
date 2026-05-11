f0c7deef743d893253b4acdfb336f9fa809be0b1
mix <mixiate@proton.me>
2026-05-11 03:36:46 +0100

    test: use multiprocessing in bmf test

--- tests/test_bmf.py
+++ tests/test_bmf.py
@@ -1,9 +1,23 @@
 """BMF format tests."""
 
+import itertools
+import multiprocessing
 from pathlib import Path
 import pytest
 
 from ts1_formats import bmf
+
+
+def roundtrip_bmf(file_path: Path, tmp_path: Path) -> None:
+    """Test reading, writing and rereading a bmf file."""
+    bmf_file = bmf.read_file(file_path)
+
+    output_file_path = tmp_path / file_path.name
+
+    bmf.write_file(output_file_path, bmf_file)
+    output_bmf_file = bmf.read_file(output_file_path)
+
+    assert bmf_file == output_bmf_file
 
 
 def test_bmf(tmp_path: Path, files_directory: str | None) -> None:
@@ -13,12 +27,5 @@ def test_bmf(tmp_path: Path, files_directory: str | None) -> None:
 
     file_list = Path(files_directory).rglob("*.bmf")
 
-    for file_path in file_list:
-        bmf_file = bmf.read_file(file_path)
-
-        output_file_path = tmp_path / file_path.name
-
-        bmf.write_file(output_file_path, bmf_file)
-        output_bmf_file = bmf.read_file(output_file_path)
-
-        assert bmf_file == output_bmf_file
+    pool = multiprocessing.Pool(None)
+    pool.starmap(roundtrip_bmf, zip(file_list, itertools.repeat(tmp_path)))
