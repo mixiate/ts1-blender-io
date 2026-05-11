e9100e0fe567a9544d51ef8f383e84e98c370a11
mix <mixiate@proton.me>
2026-05-11 16:14:06 +0100

    test: write and reread memory stream instead of file in bcf test

--- tests/test_bcf.py
+++ tests/test_bcf.py
@@ -1,6 +1,6 @@
 """BCF format tests."""
 
-import itertools
+import io
 import multiprocessing
 from pathlib import Path
 import pytest
@@ -8,19 +8,22 @@ import pytest
 from ts1_formats import bcf
 
 
-def roundtrip_bcf(file_path: Path, tmp_path: Path) -> None:
+def roundtrip_bcf(file_path: Path) -> None:
     """Test reading, writing and rereading a bcf file."""
     bcf_file = bcf.read_file(file_path)
 
-    output_file_path = tmp_path / file_path.name
+    byte_stream = io.BytesIO()
+    bcf.write_bcf(byte_stream, bcf_file)
 
-    bcf.write_file(output_file_path, bcf_file)
-    output_bcf_file = bcf.read_file(output_file_path)
+    assert byte_stream.tell() == file_path.stat().st_size
+
+    byte_stream.seek(0)
+    output_bcf_file = bcf.read_bcf(byte_stream)
 
     assert bcf_file == output_bcf_file
 
 
-def test_bcf(tmp_path: Path, files_directory: str | None) -> None:
+def test_bcf(files_directory: str | None) -> None:
     """Test reading, writing and rereading all bcf files in the specified directory."""
     if files_directory is None:
         pytest.skip("No file directory specified")
@@ -28,4 +31,4 @@ def test_bcf(tmp_path: Path, files_directory: str | None) -> None:
     file_list = Path(files_directory).rglob("*.bcf")
 
     pool = multiprocessing.Pool(None)
-    pool.starmap(roundtrip_bcf, zip(file_list, itertools.repeat(tmp_path)))
+    pool.map(roundtrip_bcf, file_list)
